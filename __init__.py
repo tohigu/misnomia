a8d3b451f9b0e2860839307c77c1603ed09dc5e8
Unknown <hello@tonyhiguchi.com>
2018-03-16 17:57:04 -0400

    Init
    Initial commit

---
+++ __init__.py
@@ -0,0 +1,78 @@
+# TODO: Add an appropriate license to your skill before publishing.  See
+# the LICENSE file for more information.
+
+# Below is the list of outside modules you'll be using in your skill.
+# They might be built-in to Python, from mycroft-core or from external
+# libraries.  If you use an external library, be sure to include it
+# in the requirements.txt file so the library is installed properly
+# when the skill gets installed later by a user.
+
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill, intent_handler
+from mycroft.util.log import LOG
+
+# Each skill is contained within its own class, which inherits base methods
+# from the MycroftSkill class.  You extend this class as shown below.
+
+# TODO: Change "Template" to a unique name for your skill
+class MisnomiaSkill(MycroftSkill):
+
+    # The constructor of the skill, which calls MycroftSkill's constructor
+    def __init__(self):
+        super(MisnomiaSkill, self).__init__(name="MisnomiaSkill")
+        
+        # Initialize working variables used within the skill.
+        self.count = 0
+
+    # The "handle_xxxx_intent" function is triggered by Mycroft when the
+    # skill's intent is matched.  The intent is defined by the IntentBuilder()
+    # pieces, and is triggered when the user's utterance matches the pattern
+    # defined by the keywords.  In this case, the match occurs when one word
+    # is found from each of the files:
+    #    vocab/en-us/Hello.voc
+    #    vocab/en-us/World.voc
+    # In this example that means it would match on utterances like:
+    #   'Hello world'
+    #   'Howdy you great big world'
+    #   'Greetings planet earth'
+    @intent_handler(IntentBuilder("").require("You").require("Hairy").require("Food"));
+    def handle_you_hairy_food_intent(self, message):
+        # In this case, respond by simply speaking a canned response.
+        # Mycroft will randomly speak one of the lines from the file
+        #    dialogs/en-us/hello.world.dialog
+        self.speak_dialog("me.bang.thing")
+        
+    @intent_handler(IntentBuilder("").require("Rock").require("Sleep").require("Big"));
+    def handle_you_rock_sleep_big(self, message):
+        # In this case, respond by simply speaking a canned response.
+        # Mycroft will randomly speak one of the lines from the file
+        #    dialogs/en-us/hello.world.dialog
+        self.speak_dialog("water.smelly.sun")
+        
+    @intent_handler(IntentBuilder("").require("Fire").require("Small").require("Go"));
+    def handle_you_fire_small_go(self, message):
+        # In this case, respond by simply speaking a canned response.
+        # Mycroft will randomly speak one of the lines from the file
+        #    dialogs/en-us/hello.world.dialog
+        self.speak_dialog("tree.cave.verisimilitude")  
+        
+    @intent_handler(IntentBuilder("").require("Wood").require("Air").require("Jupiter"));
+    def handle_you_fire_small_go(self, message):
+        # In this case, respond by simply speaking a canned response.
+        # Mycroft will randomly speak one of the lines from the file
+        #    dialogs/en-us/hello.world.dialog
+        self.speak_dialog("earth.stone.wake")           
+
+    # The "stop" method defines what Mycroft does when told to stop during
+    # the skill's execution. In this case, since the skill's functionality
+    # is extremely simple, there is no need to override it.  If you DO
+    # need to implement stop, you should return True to indicate you handled
+    # it.
+    #
+    # def stop(self):
+    #    return False
+
+# The "create_skill()" method is used to create an instance of the skill.
+# Note that it's outside the class itself.
+def create_skill():
+    return MisnomiaSkill()
